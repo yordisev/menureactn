9368616bb33c9e723ec61e1d723082d9eb013dbe
yordisev <yordys12@gmail.com>
2023-05-12 16:00:50 -0500

    listo

--- screen/Detalle.js
+++ screen/Detalle.js
@@ -1,11 +1,32 @@
-import { View, Text,StyleSheet } from 'react-native'
 import React from 'react'
+import { View, Text,StyleSheet,TouchableOpacity } from 'react-native'
+import { ALERT_TYPE, Dialog, AlertNotificationRoot, Toast } from 'react-native-alert-notification';
 
 const Detalle = () => {
+    
+  const toggleAlert = () => {
+     Dialog.show({
+          type: ALERT_TYPE.SUCCESS, //DANGER,WARNING
+          title: 'Success',
+          textBody: 'Congrats! this is dialog box success',
+          button: 'close',
+        })
+    //   Toast.show({
+    //     type: ALERT_TYPE.SUCCESS,
+    //     title: 'Verificar',
+    //     textBody: 'Los Campos no Pueden estar vacios',
+    //   })
+    }
   return (
+        <AlertNotificationRoot>
     <View style={styles.container}>
+        <TouchableOpacity onPress={toggleAlert}>
+        <Text>Tap me</Text>
+      </TouchableOpacity>
+      
       <Text>Detalle</Text>
     </View>
+  </AlertNotificationRoot>
   )
 }
 const styles = StyleSheet.create({
